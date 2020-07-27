4b2d9875ef36849838c298a77ac9d201f30b278e
Robert Zahn <rob.jzahn@gmail.com>
2020-07-27 15:56:30 -0400

    Add clg notes

---
+++ js_sandbox/clg.js
@@ -0,0 +1,29 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+const greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2 });
+
+// Log tabular info
+
+console.table({ a: 1, b: 2 });
+
+// Custom error/warning msgs
+
+console.error('This is an error message');
+console.warn('This is a warning');
+
+//Displays the runtime of code between .time and .timeEnd
+console.time('Hello');
+
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+
+console.timeEnd('Hello');
