6e1acd6628e59fb27233e48fb86bd9d091689e1a
Robert Zahn <rob.jzahn@gmail.com>
2020-07-28 19:21:09 -0400

    Add mouse event notes

---
+++ dom_manipulation/mouseEvents.js
@@ -0,0 +1,53 @@
+console.log('############');
+console.log('Mouse Events');
+console.log('############');
+
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
+
+// Click
+
+// clearBtn.addEventListener('click', runEvent);
+
+// Double click
+
+// clearBtn.addEventListener('dblclick', runEvent);
+
+// Mouse Down
+
+// clearBtn.addEventListener('mousedown', runEvent);
+
+// Mouse Up
+
+// clearBtn.addEventListener('mouseup', runEvent);
+
+// Mouse Enter
+
+// card.addEventListener('mouseenter', runEvent);
+
+// Mouse Leave
+
+// card.addEventListener('mouseleave', runEvent);
+
+// Mouse Over
+
+// card.addEventListener('mouseover', runEvent);
+
+// Mouse Out
+
+// card.addEventListener('mouseout', runEvent);
+
+// Mouse Move
+
+card.addEventListener('mousemove', runEvent);
+
+// Event Handler
+
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  heading.textContent = `MouseX: ${e.offsetX} Mouse Y: ${e.offsetY}`;
+
+  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+}
